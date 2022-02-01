2f1e8262370a9202e7514643889ce2a19272a191
Prakash Prajapat <prakash.prajapat@ibm.com>
2022-02-01 14:12:54 +0530

    Week1

--- Day3/index.js
+++
@@ -1,5 +0,0 @@
-var newElement = document.createElement("h1")
-var parElem = document.getElementById("app")
-newElement.textContent = "I am the New Element"
-console.log(newElement)
-parElem.appendChild(newElement)
