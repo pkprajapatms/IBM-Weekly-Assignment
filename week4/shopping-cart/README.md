54eb5ee1c4a7a197d079db38e0d35b0524f96b72
Prakash Prajapat <prakash.prajapat@ibm.com>
2022-02-22 09:44:34 +0530

    Week 4 assignment submission

---
+++ week4/shopping-cart/README.md
@@ -0,0 +1,5 @@
+# React Shopping Cart
+
+- CRUD Functionalities
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
---
+++ week4/shopping-cart/src/data.js
@@ -0,0 +1,41 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Sony DSC W830 Cybershot',
+        price: "11,790",
+        image: 'https://m.media-amazon.com/images/I/71bCLcUXhJL._SL1072_.jpg',
+      },
+      {
+        id: '2',
+        name: 'Noise Buds VS103',
+        price: '1,499',
+        image: 'https://m.media-amazon.com/images/I/51SehtEemXL._SL1500_.jpg',
+      },
+      {
+        id: '3',
+        name: 'Gold-Carpenters',
+        price: 429,
+        image: 'https://m.media-amazon.com/images/I/41hImSfXDYL._SL1000_.jpg',
+      },
+      {
+        id: '4',
+        name: 'Chairs',
+        price: 2000,
+        image: 'https://m.media-amazon.com/images/I/71HaD7ncUrL._SL1500_.jpg',
+      },
+      {
+        id: '5',
+        name: 'Rich Dad Poor Dad',
+        price: 399.00,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/51u8ZRDCVoL._SX330_BO1,204,203,200_.jpg',
+      },
+      {
+        id: '6',
+        name: 'Redmi 9A Sport ',
+        price: '7,499',
+        image: 'https://m.media-amazon.com/images/I/81p+EkIvUUL._SL1500_.jpg',
+      },
+    ],
+  };
+  export default data;
